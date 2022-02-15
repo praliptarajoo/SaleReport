7d7054da2bd65c6581ffebf85ede7195178b7f0f
praliptarajoo <praliptarajoo@gmail.com>
2022-02-15 17:10:18 +0530

    updating file locations

--- SaleReport/settings.py
+++ SaleReport/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static']
+STATIC_URL = 'SaleReportApp/static/'
+STATICFILES_DIRS = ['SaleReportApp/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
